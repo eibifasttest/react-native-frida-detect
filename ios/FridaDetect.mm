06d6b7d5f3d2136a63cd88144c9d9d181a1d0d9a
chonglim <chonglim.tan@ifastfinancial.com>
2024-01-22 09:28:21 +0800

    - remove ios implementation

--- ios/FridaDetect.mm
+++ ios/FridaDetect.mm
@@ -5,20 +5,12 @@
 #include <syslog.h>
 
 
-typedef int (*ptrace_ptr_t)(int _request, pid_t _pid, caddr_t _addr, int _data);
-
 @implementation FridaDetect
 RCT_EXPORT_MODULE()
 
-void anti_debug() {
-  ptrace_ptr_t ptrace_ptr = (ptrace_ptr_t)dlsym(RTLD_SELF, "ptrace");
-  ptrace_ptr(31, 0, 0, 0); // PTRACE_DENY_ATTACH = 31
-}
 
 RCT_EXPORT_METHOD(detect)
 {
-  anti_debug();
-  NSLog(@"anti_debug() trigerred");
 }
 
 
